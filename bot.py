22ab59673117bb890b8c9a76a0f9567e20b5c922
Xenzai <37379722+Raptor3500@users.noreply.github.com>
2018-10-25 18:56:38 -0400

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,21 @@
+import discord
+from discord.ext import commands
+from discord.ext.commands import Bot
+import asyncio
+import os
+import random
+import youtube_dl
+
+bot = commands.Bot(command_prefix='>')
+ownerID = "405266248314781696"
+
+# To remove the help command and make your own help command
+#bot.remove_command('help')
+
+@bot.event
+async def on_ready():
+  print ("------")
+  print ("My name is " + bot.user.name)
+  print ("With the ID: " + bot.user.id)
+  print ("Using discord.py v" + discord.__version__)
+  print ("------")
