c637f5d36b0482213fc4e12009f974d2dbe1b695
Xenzai <37379722+Raptor3500@users.noreply.github.com>
2018-10-26 12:55:53 -0400

    Update owner.py

--- cogs/owner.py
+++ cogs/owner.py
@@ -9,59 +9,48 @@ class manage():
     def __init__(self, bot):
         self.bot = bot
         
-        @commands.command(pass_context=True)
-        asynce def load(self, ctx, extension_name : str):
-            """Loads an extension."""
-            extension_name = extension_name.strip()
-            if "cogs." not in extension name:
-                extension_name = "cogs." + extension_name # nico load Owner => nico load cogs.Owner
-                if ctx.message.author.id in ownerID:
-                    try:
-                        self.bot.load_extension(extension_name)
-                        embed = discord.Embed(title=none, description=None, color=OK)
-                        embed.add_field(name=extension, value="Cog loaded.")
-                        await self.bot.say(embed=embed)
-                    except (AttributeError, ImportError) as e:
-                        embedError = discord.Embed(title=None, description=None, color=Error)
-                        embedError.add_field(name='Cog Error', value="```py\n{}: {}\n```".format(type(e).__name__, str (e)), inline=True)
-                        await self.bot.say(embed=embedError)
-                        return
-                else:
-                     noperm = discord.Embed(title=None, description=None, color=Error)
-                     noperm.add_field(name='Error', value='Are you sure you have permission to load ' + extension_name, inline=True)
-                     await self.bot.say(embed=noperm)
+    @commands.command(pass_context=True)
+    async def load(self, ctx, extension_name : str):
+        """Loads an extension."""
+        extension_name = extension_name.strip()
+        if "cogs." not in extension name:
+            extension_name = "cogs." + extension_name # nico load Owner => nico load cogs.Owner
+            if ctx.message.author.id in ownerID:
+                try:
+                    self.bot.load_extension(extension_name)
+                    embed = discord.Embed(title=none, description=None, color=OK)
+                    embed.add_field(name=extension, value="Cog loaded.")
+                    await self.bot.say(embed=embed)
+                except (AttributeError, ImportError) as e:
+                    embedError = discord.Embed(title=None, description=None, color=Error)
+                    embedError.add_field(name='Cog Error', value="```py\n{}: {}\n```".format(type(e).__name__, str (e)), inline=True)
+                    await self.bot.say(embed=embedError)
+                    return
+            else:
+                 noperm = discord.Embed(title=None, description=None, color=Error)
+                 noperm.add_field(name='Error', value='Are you sure you have permission to load ' + extension_name, inline=True)
+                 await self.bot.say(embed=noperm)
                      
-        @commands.command(pass_context=True)
-        async def load(self, ctx, extension_name : str):
-           """Unloads an extension"""
-           extension_name = extension_name.strip()
-           if "cogs." not in extension name:
-              extension_name = "cogs." = extension_name # nico unload cogs.Owner
-              if ctx.message.author.id in ownerID:
-                 if "Owner" in extension_name:
-                     embed = discord.Embed(title=None, description=None, color=Error)
-                     embed.add_field(name='Error', value=extension_name + " can't be unloaded.", inline=False)
-                     await self.bot.say(embed=embed) # This obviously stops cogs.Owner from being disabled.
-                 else:
-                      self.bot.say(embed=noperm)
-                      embed = discord.Embed(title=None, description=None, color=OK)
-                      embed.add_field(name=extension_name, value="Cog unloaded."
-                      await self.bot.say(embed=embed)
-              else:
-                   noperm = discord.Embed(title=None, description=None, color=Error)
-                   noperm.add_field(name='Error', value='Are you sure you have enough permission to unload' + extension_name, inline=True)
-                   await self.bot.say(embed=noperm)
-                   
-        @commands.command(pass_context=True)
-        async def pfp(self, ctx, args : str):
-             if ctx.message.author.id in ownerID:
-                args = args.strip()
-                if 'default' in args:
-                    with open('new aito isshoni pfp.jpg, 'rb') as f:
-                        await self.bot.edit_profile(avatar=f.read())
-                        embed = discord.Embed(title=None, description=None, color=OK)
-                        embed.add_field(name='profile', value='profile picture has changed.', inline=False)
-                        await self.bot.say(embed=embed)
+    @commands.command(pass_context=True)
+    async def load(self, ctx, extension_name : str):
+        """Unloads an extension"""
+        extension_name = extension_name.strip()
+        if "cogs." not in extension name:
+            extension_name = "cogs." = extension_name # nico unload cogs.Owner
+            if ctx.message.author.id in ownerID:
+                if "Owner" in extension_name:
+                    embed = discord.Embed(title=None, description=None, color=Error)
+                    embed.add_field(name='Error', value=extension_name + " can't be unloaded.", inline=False)
+                    await self.bot.say(embed=embed) # This obviously stops cogs.Owner from being disabled.
+                else:
+                     self.bot.say(embed=noperm)
+                     embed = discord.Embed(title=None, description=None, color=OK)
+                     embed.add_field(name=extension_name, value="Cog unloaded."
+                     await self.bot.say(embed=embed)
+            else:
+                 noperm = discord.Embed(title=None, description=None, color=Error)
+                 noperm.add_field(name='Error', value='Are you sure you have enough permission to unload' + extension_name, inline=True)
+                 await self.bot.say(embed=noperm)
 
 
 
