cce4ad947d50a2c9f6b6e2aafdb7413e8ef826c3
Xenzai <37379722+Raptor3500@users.noreply.github.com>
2018-10-25 22:25:11 -0400

    Update message.py

--- cogs/message.py
+++ cogs/message.py
@@ -14,7 +14,7 @@ class message():
         if 'pervert' in message.content:
             await self.bot.send_message(message.channel, " *attempts to kill the perv* ")
             
-        if not message.author.bot and (message.server == None or self.bot.user in message.mentions):
+        if not message.author.self.bot and (message.server == None or self.bot.user in message.mentions):
             await self.bot.send_typing(message.channel)
             txt = message.content.replace(message.server.me.mention,'') if message.server else message.content
             r = json.loads(requests.post('https://cleverbot.io/1.0/ask', json={'user':user, 'key':key, 'nick':'claire', 'text':txt}).text)
