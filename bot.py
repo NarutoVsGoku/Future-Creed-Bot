91e1277a0a5ba18ed8db179cc7952e97d5d9b8c0
NarutoVsGoku <36376796+NarutoVsGoku@users.noreply.github.com>
2018-10-26 11:16:40 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -52,31 +52,7 @@ async def say(ctx, *args):
       await bot.say(mesg)
       print (ctx.message.author.id + " or " + ctx.message.author.name + " made me say'{}'".format(mesg))
 
-  @bot.command(pass_context=True)
-  const Discord = require("discord.js");
-  const client = new Discord.client();
-  
-  const newUsers = [];
-  
-  client.on("ready", () => {console.log("I am ready!")'});
-  client.on("message", (message) => {
-    if (message.content.startsWith("ping")) {message.channel.send("!");}}];
 
-  client.on("guildMemberADD", (member) => {
-    const guild = member .guild;
-    if (!newUsers[guild.id] newUsers[guild.id] = new Discord.Collection();
-        newUsers[guild.id].set(member.id, member.user);
-        
-        if (newUsers[guild.id).size > 10) {
-           const userlist = newUsers[guild.id].map(u => u.toString()).join("");
-          guild.channels.find(channel => channel.name === "general").send("Welcome our new users!\n" + userlist);
-          newUsers[guild.id].clear();}});
-  client.on("guildMemberRemove", (member) => {
-    const guild = member.guild;
-    if (newUsers[guild.id].has(member.id))
-  newUsers.delete(member.id);});
-  client.login("SuperSecretBotTokenHere");
-   
         
         
        
