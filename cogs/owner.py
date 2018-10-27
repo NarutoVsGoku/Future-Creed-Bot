c3c936ef0fc49d91a62d1f6aceb8cab98b1c9171
Xenzai <37379722+Raptor3500@users.noreply.github.com>
2018-10-26 21:19:56 -0400

    Update owner.py

--- cogs/owner.py
+++ cogs/owner.py
@@ -3,10 +3,7 @@ from discord.ext import commands
 
 OK = 0x89f442
 Error = 0xf4141
-ownerID = [
-    '274298631517896704'
-    '436294612521582603'
-]
+ownerID = '274298631517896704' and '436294612521582603'
 
 class owner(): 
     def __init__(self, bot):
