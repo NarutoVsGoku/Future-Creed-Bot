75e1062ccc5eed8d82a31cd5c7e21415e3d198be
NarutoVsGoku <36376796+NarutoVsGoku@users.noreply.github.com>
2018-11-11 17:09:12 +0000

    Update bot.py

--- bot.py
+++ bot.py
@@ -82,23 +82,7 @@ async def ping(ctx):
   await bot.say("ping_pong: ping!! xSSS"
   print ("user has pinged")
                 
-@bot.command(pass_context=True)
-async def info(ctx, user: discord.Member):
-     await bot.say("The users name is: {}".format(user.name))       
-     await bot.say("The users ID is: {}".format(user.id))
-     await bot.say("The users status is: {}".format(user.status))  
-     await bot.say("The users highest role is: {}".format(user.top_role))   
-     await bot.say("The user joined at: {}".format(user.joined_at)) 
 
-@bot.command(pass_context=True)
-async def kick(ctx, user: discord.Member):
-    await bot.say(":boot: Cya, {}. Ya loser!".format(user.name)) 
-    await bot.kick(user) 
-
-@bot.command(pass_context=True)
-async def ban(ctx, user: discord.Member):  
-    await bot.say(":boot: Cya, {}. Ya loser!".format(user.name))
-    await bot.ban(user)
   
   
   
