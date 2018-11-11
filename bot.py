d7cca9b3c55998d83ef509c13f9b4d6d372607d6
NarutoVsGoku <36376796+NarutoVsGoku@users.noreply.github.com>
2018-11-11 16:33:18 +0000

    Update bot.py

--- bot.py
+++ bot.py
@@ -70,15 +70,35 @@ async def playing(ctx, *args):
     mesg = ''.join(args)
     await bot.change_presence(game=discord.Game(name= (mesg)))
     await bot.say("I am now playing" + mesg)
+    
+@bot.event
+async def on ready():
+  print ("Ready when you are xd")
+  print ("I am running on " + bot.user.name)
+  print ("With the ID: " + bot.user.id)
+  
+@bot.command(pass_context=True)
+async def ping(ctx):
+  await bot.say("ping_pong: ping!! xSSS"
+  print ("user has pinged")
+                
+@bot.command(pass(context=True)
+async def info(ctx, user: discord.Member):
+     await bot.say("The users name is: {}".format(user.name))       
+     await bot.say("The users ID is: {}".format(user.id))
+     await bot.say("The users status is: {}".format(user.status))  
+     await bot.say("The users highest role is: {}".format(user.top_role))   
+     await bot.say("The user joined at: {}".format(user.joined_at)) 
 
-        
-        
-       
-  
-  
-  
-  
-  
+@bot.command(pass_context=True)
+async def kick(ctx, user: discord.Member):
+    await bot.say(":boot: Cya, {}. Ya loser!".format(user.name)) 
+    await bot.kick(user) 
+
+@bot.command(pass_context=True)
+async def ban(ctx, user: discord.Member):  
+    await bot.say(":boot: Cya, {}. Ya loser!".format(user.name))
+    await bot.ban(user)
   
   
   
