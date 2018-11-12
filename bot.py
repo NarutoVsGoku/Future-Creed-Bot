40274b6086ff9113fbf6df6dfd0a8ab4502b881d
NarutoVsGoku <36376796+NarutoVsGoku@users.noreply.github.com>
2018-11-12 16:23:09 +0000

    Update bot.py

--- bot.py
+++ bot.py
@@ -84,8 +84,8 @@ async def ping(ctx):
         t1 = time.perf_counter()
         await bot.send_typing(channel)
         t2 = time.perf_counter()
-         embed=discord.Embed(title=None, description='Ping: {}'.format(round((t2-t1)*1000)), color=0x2874A6)
-         await bot.say(embed=embed)
+        embed=discord.Embed(title=None, description='Ping: {}'.format(round((t2-t1)*1000)), color=0x2874A6)
+        await bot.say(embed=embed)
 
 
 
