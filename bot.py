38eccf35b45b56c5d0c1ccfeca8ef5ef1ff57633
Xenzai <37379722+Raptor3500@users.noreply.github.com>
2018-11-12 11:58:41 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -74,7 +74,7 @@ async def playing(ctx, *args):
 @bot.command(pass_context=True)
 async def ping(ctx):
   """ Pong! """
-  await delete_message(ctx.message)
+  await bot.delete_message(ctx.message)
   before = time.monotonic()
   message = await ctx.send("Pong!")
   ping = (time.monotonic() - before) * 1000
